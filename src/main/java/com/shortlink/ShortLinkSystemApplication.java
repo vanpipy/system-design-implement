af2b9dded1a1a580355bf7e3e7e0221761d41126
vanpipy <vanpipy@gmail.com>
2026-03-31 23:41:54 +0800

    chore: add Gradle build configuration and project structure

---
+++ src/main/java/com/shortlink/ShortLinkSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortLinkSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shortlink/ShortLinkSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.shortlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.redisson.spring.starter.RedissonAutoConfigurationV2,org.redisson.spring.starter.RedissonAutoConfigurationV4"
+})
+class ShortLinkSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
